931c8568b5c13212a49eb6d6afa4205c0551a88a
oleg_eremichev <eremuchew.gelo@gmail.com>
2026-07-28 23:41:06 +0300

    Commit #4

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
@@ -8,6 +7,5 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient()
   ]
 };
